e36996992a7e8caa0c0cc1199505f9807cc2b544
blascak.milos <blascak.milos@24725613-f24a-c51b-a9fb-fcc922c7d4b8>
2012-10-30 15:39:47 +0000

    Vytvoreny model.

---
+++ trunk/protected/models/StudentLists.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * User: M. Blascak
+ * Date: 30.10.2012
+ * Time: 16:20
+ */
+class StudentLists extends CActiveRecord
+{
+
+    static public function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    public function tableName()
+    {
+        return 'tis_student_lists';
+    }
+
+    public function rules()
+    {
+        return array( 
+
+        );
+    }
+
+    public function relations()
+    {
+        return array( 
+        'user'=>array(self::BELONGS_TO, 'Users', 'user_id'),
+        'name'=>array(self::MANY_MANY, 'Students', students_lists(student_id, list_id)) 
+//         );
+    }
+
+}
---
+++ trunk/protected/models/Students.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * User: M. Blascak
+ * Date: 30.10.2012
+ * Time: 16:03
+ */
+class Students extends CActiveRecord
+{
+
+    static public function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    public function tableName()
+    {
+        return 'tis_students';
+    }
+
+    public function rules()
+    {
+        return array( 
+
+        );
+    }
+
+    public function relations()
+    {
+        return array( 
+        'user'=>array(self::BELONGS_TO, 'Users', 'user_id')
+
+        );
+    }
+
+}
