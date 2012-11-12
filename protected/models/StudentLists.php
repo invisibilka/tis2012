a34347e43e11e6f8cf363c1b628878907ed368f0
blascak.milos@gmail.com <blascak.milos@gmail.com@24725613-f24a-c51b-a9fb-fcc922c7d4b8>
2012-11-12 18:53:03 +0000

    Pridane komentare.

--- protected/models/StudentLists.php
+++ protected/models/StudentLists.php
@@ -1,25 +1,41 @@
 <?php
-
+/**
+ * Reprezentuje zoznamy študentov v databáze.
+ * M. Blascak
+ */
 class StudentLists extends CActiveRecord
 {
-
+    /**
+     * Vrati novu instanciu tejto triedy
+     * @param string $className
+     * @return CActiveRecord - instancia StudentLists
+     */
     static public function model($className = __CLASS__)
     {
         return parent::model($className);
     }
-
+    /**
+     * vrati nazov tabulky v databaze
+     * @return string nazov tabulky
+     */
     public function tableName()
     {
         return 'tis_student_lists';
     }
-
+    /**
+     * Obsahuje pravidla validacie
+     * @return array - pravidla validacie
+     */
     public function rules()
     {
         return array( 
 
         );
     }
-
+    /**
+     * Reprezentuje vztahy medzi modelmi
+     * @return array - vztahy medzi modelmi
+     */
     public function relations()
     {
         return array( 
